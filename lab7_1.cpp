cbe35e8835656912dfa3a22c4b0dbdd3deb7e01a
nmillorut <nmillorut@gmail.com>
2024-12-06 14:15:13 +0700

    lab7_1.cpp completed

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+    float a  =  20, b = 30, c = 4, output;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+    output = a - b/c;
+    std::cout << "output:" << output << "\n";
     return 0;
 }
